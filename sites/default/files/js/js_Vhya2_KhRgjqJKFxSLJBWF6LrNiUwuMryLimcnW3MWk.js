f5577fc220aac2fc4fe449edb8b57eb95d7683a2
unknown <bherrada@bherrada-nb.(none)>
2012-04-13 17:45:01 -0300

    home & portfolio

---
+++ sites/default/files/js/js_Vhya2_KhRgjqJKFxSLJBWF6LrNiUwuMryLimcnW3MWk.js
@@ -0,0 +1,150 @@
+/**
+ * @file ajaxView.js
+ *
+ * Handles AJAX fetching of views, including filter submission and response.
+ */
+(function ($) {
+
+/**
+ * Attaches the AJAX behavior to Views exposed filter forms and key View links.
+ */
+Drupal.behaviors.ViewsAjaxView = {};
+Drupal.behaviors.ViewsAjaxView.attach = function() {
+  if (Drupal.settings && Drupal.settings.views && Drupal.settings.views.ajaxViews) {
+    // Retrieve the path to use for views' ajax.
+    var ajax_path = Drupal.settings.views.ajax_path;
+
+    // If there are multiple views this might've ended up showing up multiple times.
+    if (ajax_path.constructor.toString().indexOf("Array") != -1) {
+      ajax_path = ajax_path[0];
+    }
+
+    $.each(Drupal.settings.views.ajaxViews, function(i, settings) {
+      var view = '.view-dom-id-' + settings.view_dom_id;
+      var element_settings = {
+        url: ajax_path,
+        submit: settings,
+        setClick: true,
+        event: 'click',
+        selector: view,
+        progress: { type: 'throbber' }
+      };
+
+      // Process exposed filter forms.
+      $('form#views-exposed-form-' + settings.view_name.replace(/_/g, '-') + '-' + settings.view_display_id.replace(/_/g, '-'))
+      .filter(':not(.views-processed)')
+      .each(function () {
+        var button = $('input[type=submit], input[type=image]', this);
+        button = button[0];
+
+        var ajax = new Drupal.ajax($(button).attr('id'), button, element_settings);
+      })
+      .addClass('views-processed')
+
+      $(view).filter(':not(.views-processed)')
+        // Don't attach to nested views. Doing so would attach multiple behaviors
+        // to a given element.
+        .filter(function() {
+          // If there is at least one parent with a view class, this view
+          // is nested (e.g., an attachment). Bail.
+          return !$(this).parents('.view').size();
+        })
+        .each(function() {
+          // Set a reference that will work in subsequent calls.
+          var target = this;
+          $(this)
+            .addClass('views-processed')
+            // Process pager, tablesort, and attachment summary links.
+            .find('ul.pager > li > a, th.views-field a, .attachment .views-summary a')
+            .each(function () {
+              var viewData = {};
+              // Construct an object using the settings defaults and then overriding
+              // with data specific to the link.
+              $.extend(
+                viewData,
+                settings,
+                Drupal.Views.parseQueryString($(this).attr('href')),
+                // Extract argument data from the URL.
+                Drupal.Views.parseViewArgs($(this).attr('href'), settings.view_base_path)
+              );
+
+              // For anchor tags, these will go to the target of the anchor rather
+              // than the usual location.
+              $.extend(viewData, Drupal.Views.parseViewArgs($(this).attr('href'), settings.view_base_path));
+
+              element_settings.submit = viewData;
+              var ajax = new Drupal.ajax(false, this, element_settings);
+            }); // .each function () {
+      }); // $view.filter().each
+    }); // .each Drupal.settings.views.ajaxViews
+  } // if
+};
+})(jQuery);
+;
+(function ($) {
+
+$(document).ready(function() {
+
+  // Accepts a string; returns the string with regex metacharacters escaped. The returned string
+  // can safely be used at any point within a regex to match the provided literal string. Escaped
+  // characters are [ ] { } ( ) * + ? - . , \ ^ $ # and whitespace. The character | is excluded
+  // in this function as it's used to separate the domains names.
+  RegExp.escapeDomains = function(text) {
+    return (text) ? text.replace(/[-[\]{}()*+?.,\\^$#\s]/g, "\\$&") : '';
+  }
+
+  // Attach onclick event to document only and catch clicks on all elements.
+  $(document.body).click(function(event) {
+    // Catch the closest surrounding link of a clicked element.
+    $(event.target).closest("a,area").each(function() {
+
+      var ga = Drupal.settings.googleanalytics;
+      // Expression to check for absolute internal links.
+      var isInternal = new RegExp("^(https?):\/\/" + window.location.host, "i");
+      // Expression to check for special links like gotwo.module /go/* links.
+      var isInternalSpecial = new RegExp("(\/go\/.*)$", "i");
+      // Expression to check for download links.
+      var isDownload = new RegExp("\\.(" + ga.trackDownloadExtensions + ")$", "i");
+      // Expression to check for the sites cross domains.
+      var isCrossDomain = new RegExp("^(https?|ftp|news|nntp|telnet|irc|ssh|sftp|webcal):\/\/.*(" + RegExp.escapeDomains(ga.trackCrossDomains) + ")", "i");
+
+      // Is the clicked URL internal?
+      if (isInternal.test(this.href)) {
+        // Is download tracking activated and the file extension configured for download tracking?
+        if (ga.trackDownload && isDownload.test(this.href)) {
+          // Download link clicked.
+          var extension = isDownload.exec(this.href);
+          _gaq.push(["_trackEvent", "Downloads", extension[1].toUpperCase(), this.href.replace(isInternal, '')]);
+        }
+        else if (isInternalSpecial.test(this.href)) {
+          // Keep the internal URL for Google Analytics website overlay intact.
+          _gaq.push(["_trackPageview", this.href.replace(isInternal, '')]);
+        }
+      }
+      else {
+        if (ga.trackMailto && $(this).is("a[href^=mailto:],area[href^=mailto:]")) {
+          // Mailto link clicked.
+          _gaq.push(["_trackEvent", "Mails", "Click", this.href.substring(7)]);
+        }
+        else if (ga.trackOutbound && this.href) {
+          if (ga.trackDomainMode == 2 && isCrossDomain.test(this.href)) {
+            // Top-level cross domain clicked. document.location is handled by _link internally.
+            _gaq.push(["_link", this.href]);
+          }
+          else if (ga.trackOutboundAsPageview) {
+            // Track all external links as page views after URL cleanup.
+            // Currently required, if click should be tracked as goal.
+            _gaq.push(["_trackPageview", '/outbound/' + this.href.replace(/^(https?|ftp|news|nntp|telnet|irc|ssh|sftp|webcal):\/\//i, '').split('/').join('--')]);
+          }
+          else {
+            // External link clicked.
+            _gaq.push(["_trackEvent", "Outbound links", "Click", this.href]);
+          }
+        }
+      }
+    });
+  });
+});
+
+})(jQuery);
+;
