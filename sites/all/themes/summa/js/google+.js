466bf5862370793160026dde33e9779c249c341f
Christian Pontoriero <cpontoriero@summasolutions.net>
2012-06-13 13:28:08 -0300

    responsive design

--- sites/all/themes/summa/js/google+.js
+++ sites/all/themes/summa/js/google+.js
@@ -4,4 +4,12 @@
 	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
 })();
 
-
+/*
+	// Nice Menu for custom width 
+	var size = jQuery('body').width();
+	jQuery(window).resize(function() {
+		if (size <= 520){
+			jQuery("#block-nice-menus-4").appendTo("#footer");	
+		};
+	});
+*/
