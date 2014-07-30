b481c8a9ff9b02995307a7258756e9fb1e1f0a27
dscafati <dscafati@summasolutions.net>
2014-07-30 15:00:47 -0300

    #32
    Implemented section Mission & Vision

--- sites/all/themes/summa-revamp/page--company.tpl.php
+++ sites/all/themes/summa-revamp/page--company.tpl.php
@@ -7,11 +7,12 @@
  */
 
 /* Layout style */
+drupal_add_js( drupal_get_path( 'theme', 'summa_revamp' ) . '/js/jquery.dotdotdot.min.js' );
 drupal_add_css( drupal_get_path( 'theme', 'summa_revamp' ) . '/css/company/page--company.css' );
 /* Company Description Block */
 drupal_add_css( drupal_get_path( 'theme', 'summa_revamp' ) . '/css/company/company-description/company-description.css' );
 drupal_add_js( drupal_get_path( 'theme', 'summa_revamp' ) . '/js/company/company-description/skin.js' );
-drupal_add_js( drupal_get_path( 'theme', 'summa_revamp' ) . '/js/jquery.dotdotdot.min.js' );
-
+/* Mission & Vision */
+drupal_add_css( drupal_get_path( 'theme', 'summa_revamp' ) . '/css/company/mission-vision/mission-vision.css' );
 
 include( 'templates/page.tpl.php' );
