bf63d8eb53948e427500493a6ad3b9c5c7d0d239
dscafati <dscafati@summasolutions.net>
2014-08-25 13:39:30 -0300

    #69 Implemented "Open Source Cms" page

--- sites/all/themes/summa-revamp/page--services--open-source-cms.tpl.php
+++ sites/all/themes/summa-revamp/page--services--open-source-cms.tpl.php
@@ -3,7 +3,6 @@
 /* Layout */
 drupal_add_js( drupal_get_path( 'theme', 'summa_revamp' ) . '/js/jquery.dotdotdot.min.js' );
 drupal_add_css( drupal_get_path( 'theme', 'summa_revamp' ) . '/css/services/internal/page--services.css' );
-drupal_add_css( drupal_get_path( 'theme', 'summa_revamp' ) . '/css/services/software-development/page--services--software-development.css' );
-
+drupal_add_css( drupal_get_path( 'theme', 'summa_revamp' ) . '/css/services/open-source-cms/page--services--open-source-cms.css' );
 
 include( 'templates/page.tpl.php' );
