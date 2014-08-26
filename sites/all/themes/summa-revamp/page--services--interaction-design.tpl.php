6dfb00b864b521a04abd5317a8cbe224eb4d2e23
dscafati <dscafati@summasolutions.net>
2014-08-26 14:48:47 -0300

    #71 Implemented "Interaction Desing" page

--- sites/all/themes/summa-revamp/page--services--interaction-design.tpl.php
+++ sites/all/themes/summa-revamp/page--services--interaction-design.tpl.php
@@ -5,7 +5,7 @@ drupal_add_js( drupal_get_path( 'theme', 'summa_revamp' ) . '/js/jquery.dotdotdo
 drupal_add_css( drupal_get_path( 'theme', 'summa_revamp' ) . '/css/services/internal/page--services.css' );
 drupal_add_css( drupal_get_path( 'theme', 'summa_revamp' ) . '/css/services/interaction-design/page--services--interaction-design.css' );
 /* Services Description Block */
-//drupal_add_css( drupal_get_path( 'theme', 'summa_revamp' ) . '/css/services/internal/interaction-design/services-description.css' );
+drupal_add_css( drupal_get_path( 'theme', 'summa_revamp' ) . '/css/services/internal/services-description/services-description.css' );
 drupal_add_js( drupal_get_path( 'theme', 'summa_revamp' ) . '/js/services/services-description/skin.js' );
 
 include( 'templates/page.tpl.php' );
