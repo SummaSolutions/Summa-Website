876288af8b85260387c1c2725a955cd25eac0e13
Diego Scafati <dscafati@summasolutions.net>
2014-09-10 08:47:52 -0300

    #91
    Css href corrected

--- sites/all/themes/summa-revamp/page--blogposts.tpl.php
+++ sites/all/themes/summa-revamp/page--blogposts.tpl.php
@@ -1,6 +1,6 @@
 <?php
 
 /* Layout */
-drupal_add_css( drupal_get_path( 'theme', 'summa_revamp' ) . '/css/blog/page--blogposts.css' );
+drupal_add_css( drupal_get_path( 'theme', 'summa_revamp' ) . '/css/blog/landing/page--blogposts.css' );
 
 include( 'templates/page.tpl.php' );
