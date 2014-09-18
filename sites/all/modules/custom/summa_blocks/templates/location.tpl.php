93de861343bc4214796afebef4d4aa548744d9a8
Diego Scafati <dscafati@summasolutions.net>
2014-09-18 15:55:56 -0300

    #106
    Implemented  Google Maps Api  in block Location

--- sites/all/modules/custom/summa_blocks/templates/location.tpl.php
+++ sites/all/modules/custom/summa_blocks/templates/location.tpl.php
@@ -3,11 +3,13 @@ drupal_add_css( drupal_get_path( "module", "summa_blocks" ) . "/templates/css/lo
 drupal_add_css( drupal_get_path( "theme", "summa_revamp" ) . "/css/jquery.bxslider.css" );
 drupal_add_js( drupal_get_path( "theme", "summa_revamp" ) . "/js/jquery.bxslider.min.js" );
 drupal_add_js( drupal_get_path( "module", "summa_blocks" ) . "/templates/js/location/sliderControl.js" );
+drupal_add_js("http://maps.googleapis.com/maps/api/js?key=AIzaSyAinJdoSrNxKW3k-8ArbioXeLT2HLl54gw&sensor=false");
+
 ?>
 
 <div class="location-container">
     <div class="bxslider">
-        <div class="slide">
+        <div class="slide" style="position:relative" data-latitude="-34.5862644" data-longitude="-58.4422086" data-zoom="17">
             <div class="container">
                 <div class="tablet-left">
                     <div class="pointer">
@@ -27,9 +29,8 @@ drupal_add_js( drupal_get_path( "module", "summa_blocks" ) . "/templates/js/loca
                     </div>
                 </div>
             </div>
-            <div class="background-map map-bsas bottom"></div>
         </div>
-        <div class="slide">
+        <div class="slide" style="position:relative" data-latitude="-37.320795" data-longitude="-59.1327"  data-zoom="17">
             <div class="container">
                 <div class="tablet-left">
                     <div class="pointer">
@@ -49,7 +50,8 @@ drupal_add_js( drupal_get_path( "module", "summa_blocks" ) . "/templates/js/loca
                     </div>
                 </div>
             </div>
-            <div class="background-map map-tandil bottom"></div>
         </div>
+    </div>
+    <div class="google-maps-container">
     </div>
 </div>
