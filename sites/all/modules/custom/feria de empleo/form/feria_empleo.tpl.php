cc4c5ba7e9b891548cb8e962518db1bcdf2c9efa
Diego Scafati <dscafati@summasolutions.net>
2014-10-08 18:41:43 -0300

    Compiled css

--- sites/all/modules/custom/feria de empleo/form/feria_empleo.tpl.php
+++ sites/all/modules/custom/feria de empleo/form/feria_empleo.tpl.php
@@ -30,24 +30,25 @@
         </div>
         <div class="row clearfix">
             <div class="float-left">
-                <label>Experiencia laboral en el área de sistemas</label>
+                <label>Conocimientos</label>
                 <ul>
                     <li><input type="checkbox" autocomplete="off" name="conocimientos[]" value="php">PHP</li>
                     <li><input type="checkbox" autocomplete="off" name="conocimientos[]" value="java">JAVA</li>
                     <li><input type="checkbox" autocomplete="off" name="conocimientos[]" value="html5">HTML5</li>
                     <li><input type="checkbox" autocomplete="off" name="conocimientos[]" value="css3">CSS3</li>
                     <li><input type="checkbox" autocomplete="off" name="conocimientos[]" value="javascript">JAVASCRIPT</li>
-                    <li><input type="checkbox" autocomplete="off" name="conocimientos][" value="mysql">MYSQL</li>
-                    <li><input type="checkbox" autocomplete="off" name="conocimientos][" value="sass">SASS/LESS</li>
-                    <li><input type="checkbox" autocomplete="off" name="conocimientos][" value="magento">Magento</li>
+                    <li><input type="checkbox" autocomplete="off" name="conocimientos[]" value="mysql">MYSQL</li>
+                    <li><input type="checkbox" autocomplete="off" name="conocimientos[]" value="sass">SASS/LESS</li>
+                    <li><input type="checkbox" autocomplete="off" name="conocimientos[]" value="magento">Magento</li>
                     <li><input type="checkbox" autocomplete="off" name="conocimientos[]" value="hybris">Hybris</li>
+                    <li><input type="checkbox" autocomplete="off" name="conocimientos[]" value="oop">Programacion Orientada Objetos</li>
                 </ul>
             </div>
             <div class="float-right">
                 <input type="text" autocomplete="off" name="carrera" placeholder="CARRERA">
                 <input type="text" autocomplete="off" name="ano_cursada" placeholder="AÑO DE CURSADA">
                 <div class="combobox-container clearfix">
-                    <select data-validation="required" autocomplete="off" name="experiencia" placeholder="EXPERIENCIA LABORAL (*)">
+                    <select data-validation="required" autocomplete="off" name="experiencia" placeholder="EXPERIENCIA LABORAL EN EL AREA DE SISTEMAS (*)">
                         <option value="si">SI</option>
                         <option value="no">NO</option>
                     </select>
