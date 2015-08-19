874da6ea7aaabc307791e3a837f6b2f87ed7f7bd
mgonzalez <mgonzalez@mgonzalez-M68MT-S2>
2015-08-19 14:54:21 -0300

    avoid issues with scroll and maps api limit

--- sites/all/modules/custom/summa_blocks/templates/js/location/sliderControl.js
+++ sites/all/modules/custom/summa_blocks/templates/js/location/sliderControl.js
@@ -13,46 +13,20 @@ jQuery(window).load(function () {
     var longitude = firstSlide.data("longitude");
     var zoom = firstSlide.data("zoom");
 
-    var latlng = new google.maps.LatLng( latitude, longitude );
-
-    var mapOptions = {
-        center: latlng,
-        zoom: zoom,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        mapTypeControl : false,
-        maxZoom : 19,
-        zoomControl: true,
-        zoomControlOptions: {
-            style: google.maps.ZoomControlStyle.SMALL
-        }
-    };
-
-    var map =  new google.maps.Map( mapElement.get(0), mapOptions );
-    mapElement.data("map", map);
-
-    // Markers for next slides
-    jQuery(".location-container .slide").each( function(){
-        var latlng = new google.maps.LatLng( jQuery(this).data("latitude"), jQuery(this).data("longitude") );
-
-        var marker = new google.maps.Marker({
-            position: latlng,
-            map: map
-        });
-    });
-
-
     locControl = jQuery(locSliderSelector).bxSlider({
         wrapperClass: 'bx-wrapper bx-wrapper-green',
         controls: false,
         mode: 'fade',
-        speed: 500,
+        speed: 1500,
+        pause: 7000,
         auto: true,
         preventDefaultSwipeX: true,
         autoHover: true,
-        onSlideAfter : function($slideElement){
+        onSlideNext : function($slideElement){
             var lat = $slideElement.data("latitude");
             var lon = $slideElement.data("longitude");
-            jQuery(locMapSelector).data("map").panTo( new google.maps.LatLng( lat, lon ) );
+            //jQuery(locMapSelector).data("map").panTo( new google.maps.LatLng( lat, lon ) );
+            jQuery(locMapSelector).find('.map').fadeToggle();
         }
     });
 
