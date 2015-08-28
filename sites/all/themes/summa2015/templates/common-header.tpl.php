827a4d2fe8812317f4cf0dfa55e35472b57826e1
mgonzalez <mgonzalez@mgonzalez-M68MT-S2>
2015-08-28 13:03:04 -0300

    #191 stiky bar

--- sites/all/themes/summa2015/templates/common-header.tpl.php
+++ sites/all/themes/summa2015/templates/common-header.tpl.php
@@ -28,3 +28,31 @@
         </div>
     </div>
 </div>
+<script type="text/javascript">
+    // Create a clone of the menu, right next to original.
+    var topBar = '#header-middle';
+    jQuery(topBar).addClass('original').clone().insertAfter(topBar).addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
+
+    scrollIntervalID = setInterval(stickIt, 10);
+
+    function stickIt() {
+
+        var orgElementPos = jQuery(topBar+'.original').offset();
+        var orgElementTop = orgElementPos.top + 25;
+
+        if (jQuery(window).scrollTop() >= (orgElementTop)) {
+            // scrolled past the original position; now only show the cloned, sticky element.
+            // Cloned element should always have same left position and width as original element.
+            var orgElement = jQuery(topBar+'.original');
+            var coordsOrgElement = orgElement.offset();
+            var leftOrgElement = coordsOrgElement.left;
+            var widthOrgElement = orgElement.css('width');
+            jQuery(topBar+'.cloned').css('left',leftOrgElement+'px').css('top',0).css('width',widthOrgElement).show();
+            jQuery(topBar+'.original').css('visibility','hidden');
+        } else {
+            // not scrolled past the menu; only show the original menu.
+            jQuery(topBar+'.cloned').hide();
+            jQuery(topBar+'.original').css('visibility','visible');
+        }
+    }
+</script>
