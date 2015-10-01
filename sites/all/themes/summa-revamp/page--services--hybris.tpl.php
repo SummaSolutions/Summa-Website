a6e78235e2fa66661541e8f9012ef0cc31cf9c9a
mgonzalez <mgonzalez@mgonzalez-M68MT-S2>
2015-10-01 08:35:19 -0300

    #191 960px width fixes

--- sites/all/themes/summa-revamp/page--services--hybris.tpl.php
+++ sites/all/themes/summa-revamp/page--services--hybris.tpl.php
@@ -8,4 +8,4 @@ drupal_add_css( drupal_get_path( 'theme', 'summa_revamp' ) . '/css/services/e-co
 drupal_add_css( drupal_get_path( 'theme', 'summa_revamp' ) . '/css/services/internal/services-description/services-description.css' );
 drupal_add_js( drupal_get_path( 'theme', 'summa_revamp' ) . '/js/services/services-description/skin.js' );
 
-include( 'templates/page.tpl.php' );
+include( summa_revamp_getThemeRealpath().'templates/page.tpl.php' );
